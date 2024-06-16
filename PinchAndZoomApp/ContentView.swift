fc080b18d3b937412b92e8a7493f0b2ea21771f4
Nicholas Stamoulis <135749660+stamos11@users.noreply.github.com>
2024-06-16 21:44:35 +0300

    Initial Commit

---
+++ PinchAndZoomApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinchAndZoomApp
+//
+//  Created by stamoulis nikolaos on 16/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinchAndZoomApp/PinchAndZoomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAndZoomAppApp.swift
+//  PinchAndZoomApp
+//
+//  Created by stamoulis nikolaos on 16/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchAndZoomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
